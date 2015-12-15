0dca248b0c657be72cf5f4e1ad8ba4c477810d56
yiyc2008 <Berton@berton-macbook-pro.local>
2015-12-15 22:04:14 +0800

    Initial Commit

---
+++ YC_ScrollNav/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YC_ScrollNav
+//
+//  Created by Berton on 15/12/15.
+//  Copyright © 2015年 Berton. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ YC_ScrollNav/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YC_ScrollNav
+//
+//  Created by Berton on 15/12/15.
+//  Copyright © 2015年 Berton. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
